db4ee47e7c278b04b4d6cd606ee6e3ef8fc6ca67
Dan Heron <dan@heron.li>
2022-11-15 17:49:09 +0100

    Changed to use MudBazor.Docs

--- Heron.MudCalendar.Docs/Program.cs
+++ Heron.MudCalendar.Docs/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Heron.MudCalendar.Docs;
 using Heron.MudCalendar.Docs.Services;
+using MudBlazor.Docs.Extensions;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -10,6 +11,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+builder.Services.TryAddDocsViewServices();
 
 builder.Services.AddScoped<EventService>();
 
