ab6c05f35fd2004f2b00cacddc1c8b6fff68988d
Dan Heron <dan@heron.li>
2023-01-25 07:45:35 +0100

    Added additional tests

--- Heron.MudCalendar.UnitTests/Components/CalendarTests.cs
+++ Heron.MudCalendar.UnitTests/Components/CalendarTests.cs
@@ -1,4 +1,6 @@
+using FluentAssertions;
 using Heron.MudCalendar.UnitTests.Viewer.TestComponents.Calendar;
+using MudBlazor;
 
 namespace Heron.MudCalendar.UnitTests.Components;
 
@@ -10,5 +12,35 @@ public class CalendarTests : BunitTest
         var cut = Context.RenderComponent<CalendarTest>();
 
         var div = cut.Find("div.mud-calendar");
+    }
+
+    [Test]
+    public void ChangeViews()
+    {
+        var cut = Context.RenderComponent<CalendarTest>();
+        cut.Find("div.mud-cal-month-view");
+
+        var comp = cut.FindComponent<EnumSwitch<CalendarView>>();
+        var buttons = comp.FindAll("button");
+        Assert.AreEqual(3, buttons.Count);
+        
+        buttons[1].Click();
+        cut.Find("div.mud-cal-week-view");
+
+        buttons[2].Click();
+        cut.Find("div.mud-cal-week-view");
+    }
+
+    [Test]
+    public void RestrictViews()
+    {
+        var cut = Context.RenderComponent<CalendarTest>();
+        var comp = cut.FindComponent<MudCalendar<CalendarItem>>();
+
+        comp.SetParam(x => x.ShowDay, false);
+        comp.SetParam(x => x.ShowWeek, false);
+
+        comp.FindComponent<EnumSwitch<CalendarView>>().Find("div").ClassList.Should().Contain("d-none");
+        cut.Find("div.mud-cal-month-view");
     }
 }
