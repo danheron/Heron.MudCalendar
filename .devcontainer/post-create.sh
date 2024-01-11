d83d866a0184b60692f48a86e2c329de4b8e8bd8
Dan Heron <dan@heron.li>
2024-01-11 11:51:03 +0100

    Updates to dev container config

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,8 +2,9 @@
 wget https://packages.microsoft.com/config/debian/11/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
-sudo apt update && \
-    sudo apt install -y dotnet-sdk-6.0
+sudo apt-get update && \
+    export DEBIAN_FRONTEND=noninteractive && \
+    sudo apt-get install -y dotnet-sdk-6.0 dotnet-sdk-7.0
 
 ## Configure git
 git config --global core.autocrlf input
@@ -11,8 +12,11 @@ git config --global core.autocrlf input
 ## Enable local HTTPS for .NET
 dotnet dev-certs https --trust
 
+# Set vscode user as owner of workspaces folder
+sudo chown vscode /workspaces
+
 # Clone and checkout MudBlazor mudcalendar branch and restore
-gh repo clone danheron/MudBlazor /workspaces/MudBlazor
+git clone https://github.com/danheron/MudBlazor /workspaces/MudBlazor
 cd /workspaces/MudBlazor
 git checkout mudcalendar
 cd src
@@ -20,7 +24,7 @@ dotnet restore
 
 ## Restore MudCalendar
 cd /workspaces/Heron.MudCalendar
-dotnet restore
+dotnet restore Heron.MudCalendarWithDocs.sln
 
 # Build debug and release versions of MudCalendar
 dotnet build /workspaces/Heron.MudCalendar/Heron.MudCalendar -c Debug --no-restore
@@ -30,7 +34,7 @@ dotnet build /workspaces/Heron.MudCalendar/Heron.MudCalendar -c Release --no-res
 dotnet build /workspaces/MudBlazor/src/MudBlazor.Docs -c Release --no-restore
 
 # Build MudCalendar projects
-dotnet build /workspaces/Heron.MudCalendar
+dotnet build /workspaces/Heron.MudCalendar/Heron.MudCalendarWithDocs.sln
 
 ## AZURE CLI EXTENSIONS ##
 # Uncomment the below to install Azure CLI extensions
@@ -62,12 +66,12 @@ dotnet build /workspaces/Heron.MudCalendar
 
 ## OH-MY-ZSH PLUGINS & THEMES (POWERLEVEL10K) ##
 # Uncomment the below to install oh-my-zsh plugins and themes (powerlevel10k) without dotfiles integration
-git clone https://github.com/zsh-users/zsh-completions.git $HOME/.oh-my-zsh/custom/plugins/zsh-completions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+#git clone https://github.com/zsh-users/zsh-completions.git $HOME/.oh-my-zsh/custom/plugins/zsh-completions
+#git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+#git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
 # git clone https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k --depth=1
-ln -s $HOME/.oh-my-zsh/custom/themes/powerlevel10k/powerlevel10k.zsh-theme $HOME/.oh-my-zsh/custom/themes/powerlevel10k.zsh-theme
+#ln -s $HOME/.oh-my-zsh/custom/themes/powerlevel10k/powerlevel10k.zsh-theme $HOME/.oh-my-zsh/custom/themes/powerlevel10k.zsh-theme
 
 ## OH-MY-POSH ##
 # Uncomment the below to install oh-my-posh
