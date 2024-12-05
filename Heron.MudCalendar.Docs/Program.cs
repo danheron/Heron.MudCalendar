4eecc941c3e4bcc7996bc50b1a86839ce735893f
Dan Heron <dan@heron.li>
2024-12-05 16:04:34 +0100

    Updated to MudBlazor 8 preview and .Net 9

--- Heron.MudCalendar.Docs/Program.cs
+++ Heron.MudCalendar.Docs/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Heron.MudCalendar.Docs;
 using Heron.MudCalendar.Docs.Services;
 using MudBlazor.Docs.Extensions;
+using MudBlazor.Docs.Services;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -11,7 +12,12 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
-builder.Services.TryAddDocsViewServices();
+//builder.Services.TryAddDocsViewServices();
+
+builder.Services.AddScoped<IDocsJsApiService, DocsJsApiService>();
+builder.Services.AddSingleton<IMenuService, MenuService>();
+builder.Services.AddScoped<IDocsNavigationService, DocsNavigationService>();
+builder.Services.AddSingleton<IRenderQueueService, RenderQueueService>();
 
 builder.Services.AddScoped<EventService>();
 
