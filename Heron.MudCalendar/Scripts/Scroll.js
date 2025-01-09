4de71566b772f9d576878ca9f1aa5a55f5fae20d
Dan Heron <dan@heron.li>
2025-01-09 08:29:15 +0100

    Refresh calendar when stylesheet loads

--- Heron.MudCalendar/Scripts/Scroll.js
+++ Heron.MudCalendar/Scripts/Scroll.js
@@ -1,15 +1,3 @@
 export function scroll(element, top) {
     element.scrollTo(0, top);
 }
-
-export function getHeadContent() {
-    return document.head.innerHTML;
-}
-
-export function addLink(href, rel) {
-    const link = document.createElement("link");
-    link.href = href;
-    link.rel = rel;
-    
-    document.head.appendChild(link);
-}
