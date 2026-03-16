eae5c0a9aef1d49d4212de88ecc713bdd88f2e3c
Dan Heron <dan@heron.li>
2026-03-16 09:05:48 +0100

    Added analytics to Docs site

--- Heron.MudCalendar.Docs/Program.cs
+++ Heron.MudCalendar.Docs/Program.cs
@@ -1,8 +1,10 @@
+using Blazor.Analytics;
+using BytexDigital.Blazor.Components.CookieConsent;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Heron.MudCalendar.Docs;
 using Heron.MudCalendar.Docs.Services;
-using MudBlazor.Docs.Extensions;
+using MudBlazor.Docs.Pages.Consent.Prompt;
 using MudBlazor.Docs.Services;
 using MudBlazor.Services;
 
@@ -18,6 +20,47 @@ builder.Services.AddScoped<IDocsJsApiService, DocsJsApiService>();
 builder.Services.AddSingleton<IMenuService, MenuService>();
 builder.Services.AddScoped<IDocsNavigationService, DocsNavigationService>();
 builder.Services.AddSingleton<IRenderQueueService, RenderQueueService>();
+
+builder.Services.AddGoogleAnalytics("G-QMN1W7PK2X");
+builder.Services.AddCookieConsent(options =>
+{
+    options.ImportJsAutomatically = false;
+    options.Revision = 1;
+    options.PolicyUrl = "/mudcalendar/cookie-policy";
+
+    // Replace default prompt. We don't use the modal.
+    options.ConsentPromptVariant = new MudCookieConsentPromptVariant();
+
+    options.Categories.Add(new CookieCategory
+    {
+        TitleText = new()
+        {
+            ["en"] = "Google Services",
+        },
+        DescriptionText = new()
+        {
+            ["en"] = "Allows the integration and usage of Google services.",
+        },
+        Identifier = "google",
+        IsPreselected = true,
+        Services =
+        [
+            new CookieCategoryService
+            {
+                Identifier = "google-analytics",
+                PolicyUrl = "https://policies.google.com/privacy",
+                TitleText = new()
+                {
+                    ["en"] = "Google Analytics",
+                },
+                ShowPolicyText = new()
+                {
+                    ["en"] = "Display policies",
+                }
+            }
+        ]
+    });
+});
 
 builder.Services.AddScoped<EventService>();
 
